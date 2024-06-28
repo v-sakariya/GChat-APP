a274cdb55f4b8e8bce972404582745b753c78f97
Vivek <sakariyavivek12@gmail.com>
2024-06-28 17:35:05 -0300

    Final Commit Message for the Chat App

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,7 @@
+export const host = "http://localhost:4000";
+export const registerRoute = `${host}/api/auth/register`
+export const loginRoute = `${host}/api/auth/login`
+export const setAvatarRoute = `${host}/api/auth/setAvatar`
+export const allUsersRoutes = `${host}/api/auth/allUsers`
+export const sendMessageRoutes =   `${host}/api/messages/addmsg`
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`
